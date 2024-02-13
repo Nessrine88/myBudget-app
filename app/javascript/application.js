875f18974cdb4c171b3778548701aa86f827b2dc
unknown <macherki.nessrine@yahoo.com>
2024-02-13 19:47:52 +0100

    add cancancan

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,3 @@
-// Entry point for the build script in your package.json
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
-
-// Using CDN
-import 'jquery/dist/jquery'
-
+import "controllers"
